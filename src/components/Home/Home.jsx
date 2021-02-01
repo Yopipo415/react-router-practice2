a003a7924c7b91d30394b1aa734ca3c4cc6bb5f4
Masaya Fukai <f.m.11404@gmail.com>
2021-02-01 21:44:45 +0900

    Exercise 6 : Create App, Home, Quiz.

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import Button from "../Button/Button";
+
+const Home = ({ history }) => {
+    return (
+        <div>
+            <h1>Welcome to the Quiz App!</h1>
+            {/* <br/>
+            <br/>
+            <Button 
+                onClickHandler={() => {
+                    history.push('/quiz');
+                }}
+            >
+                Let's start!!
+            </Button> */}
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,130 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import QuizModel from '../../models/Quiz';
+// import Button from "../Button/Button";
+// import './Quiz.css';
+
+class Quiz extends React.Component {
+    // constructor (props) {
+    //     super(props);
+    //     this.state = {
+    //         quizzes : [],
+    //         currentIndex : 0,
+    //         numberOfCorrects : 0
+    //     };
+    // }
+
+    // async componentDidMount () {
+    //     await this.restart();
+    // }
+
+    // async restart () {
+    //     this.setState({
+    //         quizzes : [],
+    //         currentIndex : 0,
+    //         numberOfCorrects : 0
+    //     });
+
+    //     const quizzes = await QuizModel.fetchAndCreateQuizzes();
+
+    //     this.setState({ quizzes });
+
+    // }
+
+    // selectAnswer (quiz, answer) {
+    //     let { currentIndex, numberOfCorrects } = this.state;
+    //     const isCorrect = quiz.judgeCorrectAnswer(answer);
+
+    //     if (isCorrect) {
+    //         numberOfCorrects ++;
+    //         alert('Correct answer!!');
+    //     } else {
+    //         alert(`Wrong answer... (The correct answer is "${quiz.correctAnswer}")`);
+    //     }
+    //     currentIndex++;
+
+    //     this.setState({
+    //         currentIndex,
+    //         numberOfCorrects
+    //     });
+    // }
+
+    // render () {
+    //     const { quizzes, currentIndex } = this.state;
+
+    //     if (quizzes.length === 0) {
+    //         return this.renderLoading();
+    //     }
+
+    //     if (quizzes.length > 0 && currentIndex < quizzes.length) {
+    //         return this.renderQuiz();
+    //     }
+
+    //     if (quizzes.length > 0 && currentIndex >= quizzes.length) {
+    //         return this.renderResult();
+    //     }
+    // }
+
+    // renderLoading () {
+    //     return (
+    //         <div>
+    //             <h1>Please wait...</h1>
+    //             <p>Now loading...</p>
+    //             <hr/>
+    //             <Link to="/">Back to the Top</Link>
+    //         </div>
+    //     );
+    // }
+
+    // renderQuiz () {
+    //     const { quizzes, currentIndex } = this.state;
+
+    //     const quiz = quizzes[currentIndex];
+    //     const answers = quiz.shuffleAnswers().map((answer,index) => {
+    //         return (
+    //             <li key={index}>
+    //                 <Button
+    //                     onClickHandler={() => { this.selectAnswer(quiz, answer) }}
+    //                 >
+    //                     {answer}
+    //                 </Button>
+    //             </li>
+    //         );
+    //     });
+
+    //     return (
+    //         <div>
+    //             <h1>Question : { currentIndex + 1 }</h1>
+    //             <div>
+    //                 <p>{quiz.question}</p>
+    //                 <ul className="QuizList">{answers}</ul>
+    //             </div>
+    //             <hr/>
+    //             <Link to="/">Back to the Top</Link>
+    //         </div>
+    //     );
+    // }
+
+    // renderResult () {
+    //     const { quizzes, numberOfCorrects } = this.state;
+
+    //     return (
+    //         <div>
+    //             <h1>Your result is...</h1>
+    //             <div>
+    //                 <p id="result">{`${numberOfCorrects}/${quizzes.length} corrects.`}</p>
+    //                 <Button
+    //                     onClickHandler={() => {this.restart()}}
+    //                 >
+    //                     Restart?
+    //                 </Button>
+    //             </div>
+    //             <hr/>
+    //             <Link to="/">Back to the Top</Link>
+    //         </div>
+    //     );
+    // }
+
+}
+
+export default Quiz;
