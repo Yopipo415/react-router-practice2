1b73237d1f24b43e57fbdfa4123341e63e301cff
Masaya Fukai <f.m.11404@gmail.com>
2021-02-01 22:08:50 +0900

    Exercise 8 : Create Quiz.

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,109 +1,109 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
-// import QuizModel from '../../models/Quiz';
-// import Button from "../Button/Button";
+import QuizModel from '../../models/Quiz';
+import Button from "../Button/Button";
 // import './Quiz.css';
 
 class Quiz extends React.Component {
-    // constructor (props) {
-    //     super(props);
-    //     this.state = {
-    //         quizzes : [],
-    //         currentIndex : 0,
-    //         numberOfCorrects : 0
-    //     };
-    // }
+    constructor (props) {
+        super(props);
+        this.state = {
+            quizzes : [],
+            currentIndex : 0,
+            numberOfCorrects : 0
+        };
+    }
 
-    // async componentDidMount () {
-    //     await this.restart();
-    // }
+    async componentDidMount () {
+        await this.restart();
+    }
 
-    // async restart () {
-    //     this.setState({
-    //         quizzes : [],
-    //         currentIndex : 0,
-    //         numberOfCorrects : 0
-    //     });
+    async restart () {
+        this.setState({
+            quizzes : [],
+            currentIndex : 0,
+            numberOfCorrects : 0
+        });
 
-    //     const quizzes = await QuizModel.fetchAndCreateQuizzes();
+        const quizzes = await QuizModel.fetchAndCreateQuizzes();
 
-    //     this.setState({ quizzes });
+        this.setState({ quizzes });
 
-    // }
+    }
 
-    // selectAnswer (quiz, answer) {
-    //     let { currentIndex, numberOfCorrects } = this.state;
-    //     const isCorrect = quiz.judgeCorrectAnswer(answer);
+    selectAnswer (quiz, answer) {
+        let { currentIndex, numberOfCorrects } = this.state;
+        const isCorrect = quiz.judgeCorrectAnswer(answer);
 
-    //     if (isCorrect) {
-    //         numberOfCorrects ++;
-    //         alert('Correct answer!!');
-    //     } else {
-    //         alert(`Wrong answer... (The correct answer is "${quiz.correctAnswer}")`);
-    //     }
-    //     currentIndex++;
+        if (isCorrect) {
+            numberOfCorrects ++;
+            alert('Correct answer!!');
+        } else {
+            alert(`Wrong answer... (The correct answer is "${quiz.correctAnswer}")`);
+        }
+        currentIndex++;
 
-    //     this.setState({
-    //         currentIndex,
-    //         numberOfCorrects
-    //     });
-    // }
+        this.setState({
+            currentIndex,
+            numberOfCorrects
+        });
+    }
 
-    // render () {
-    //     const { quizzes, currentIndex } = this.state;
+    render () {
+        const { quizzes, currentIndex } = this.state;
 
-    //     if (quizzes.length === 0) {
-    //         return this.renderLoading();
-    //     }
+        if (quizzes.length === 0) {
+            return this.renderLoading();
+        }
 
-    //     if (quizzes.length > 0 && currentIndex < quizzes.length) {
-    //         return this.renderQuiz();
-    //     }
+        if (quizzes.length > 0 && currentIndex < quizzes.length) {
+            return this.renderQuiz();
+        }
 
-    //     if (quizzes.length > 0 && currentIndex >= quizzes.length) {
-    //         return this.renderResult();
-    //     }
-    // }
+        if (quizzes.length > 0 && currentIndex >= quizzes.length) {
+            return this.renderResult();
+        }
+    }
 
-    // renderLoading () {
-    //     return (
-    //         <div>
-    //             <h1>Please wait...</h1>
-    //             <p>Now loading...</p>
-    //             <hr/>
-    //             <Link to="/">Back to the Top</Link>
-    //         </div>
-    //     );
-    // }
+    renderLoading () {
+        return (
+            <div>
+                <h1>Please wait...</h1>
+                <p>Now loading...</p>
+                <hr/>
+                {/* <Link to="/">Back to the Top</Link> */}
+            </div>
+        );
+    }
 
-    // renderQuiz () {
-    //     const { quizzes, currentIndex } = this.state;
+    renderQuiz () {
+        const { quizzes, currentIndex } = this.state;
 
-    //     const quiz = quizzes[currentIndex];
-    //     const answers = quiz.shuffleAnswers().map((answer,index) => {
-    //         return (
-    //             <li key={index}>
-    //                 <Button
-    //                     onClickHandler={() => { this.selectAnswer(quiz, answer) }}
-    //                 >
-    //                     {answer}
-    //                 </Button>
-    //             </li>
-    //         );
-    //     });
+        const quiz = quizzes[currentIndex];
+        const answers = quiz.shuffleAnswers().map((answer,index) => {
+            return (
+                <li key={index}>
+                    <Button
+                        onClickHandler={() => { this.selectAnswer(quiz, answer) }}
+                    >
+                        {answer}
+                    </Button>
+                </li>
+            );
+        });
 
-    //     return (
-    //         <div>
-    //             <h1>Question : { currentIndex + 1 }</h1>
-    //             <div>
-    //                 <p>{quiz.question}</p>
-    //                 <ul className="QuizList">{answers}</ul>
-    //             </div>
-    //             <hr/>
-    //             <Link to="/">Back to the Top</Link>
-    //         </div>
-    //     );
-    // }
+        return (
+            <div>
+                <h1>Question : { currentIndex + 1 }</h1>
+                <div>
+                    <p>{quiz.question}</p>
+                    <ul className="QuizList">{answers}</ul>
+                </div>
+                <hr/>
+                {/* <Link to="/">Back to the Top</Link> */}
+            </div>
+        );
+    }
 
     // renderResult () {
     //     const { quizzes, numberOfCorrects } = this.state;
